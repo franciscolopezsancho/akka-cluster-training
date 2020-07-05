52d09412e10cce56bb492243e380516a1bfea442
franciscolopezsancho <franciscolopezsancho@gmail.com>
2020-07-05 18:15:53 +0200

    004_cluster ready

--- exercise_003_typed_remoting/src/main/scala/example/Worker.scala
+++ exercise_003_typed_remoting/src/main/scala/example/Worker.scala
@@ -6,42 +6,25 @@ import akka.actor.typed._
 import akka.actor.typed.receptionist.Receptionist
 import akka.actor.typed.receptionist.ServiceKey
 
-
 object Worker {
 
   val WorkerKey = ServiceKey[Command]("worker-key")
 
-  def apply(): Behavior[Command]=
-    Behaviors.setup { context => 
-      context.system.receptionist ! Receptionist.Register(WorkerKey, context.self)
-      Behaviors.receiveMessage[Command] { 
-          case Dig => {
-            context.log.info(s"I'm ${context.self.path} digging")
-            Behaviors.same
-          }
+  def apply(): Behavior[Command] =
+    Behaviors.setup { context =>
+      context.system.receptionist ! Receptionist.Register(
+        WorkerKey,
+        context.self
+      )
+      Behaviors.receiveMessage[Command] {
+        case Dig => {
+          context.log.info(s"I'm ${context.self.path}, and I'm digging")
+          Behaviors.same
+        }
       }
     }
 
   sealed trait Command
   case object Dig extends Command
 
-}
-
-
-
-object Factory {
-
-  sealed trait Command
-  case object ContractWorker extends Command
-
-  def apply(): Behavior[Command] = {
-    Behaviors.receive[Command] { (context, message) => 
-      message match {
-        case ContractWorker => 
-           context.spawn(Worker(),s"worker-${scala.util.Random.nextInt(4)}")
-           Behaviors.same
-      }
-          
-    }
-  }
 }
